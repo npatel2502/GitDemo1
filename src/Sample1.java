abdb036aa41c2c383063df1313bae2225a786bb5
LENOVO <LENOVO@192.168.0.18>
2021-11-25 19:09:52 -0500

    Test

--- src/Sample1.java
+++ src/Sample1.java
@@ -3,9 +3,9 @@ public class Sample1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
 		
 		System.out.println("Hello World");
+		System.out.println("Good morning");
 		
 	}
 
